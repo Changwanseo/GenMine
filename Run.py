c045ec7f0cfdd7a30d3e85ae27d119838ba68fc5
Changwanseo <wan101010@snu.ac.kr>
2021-04-30 10:27:06 +0900

    0.7.0
    Argument parsing added!

--- Run.py
+++ Run.py
@@ -1,23 +1,52 @@
 from lib import GenMiner as Gen
+import argparse
+from datetime import datetime
 
 import os
 
+# Default arguments
 email = "wan101010@snu.ac.kr"
 genus_term = "Umbelopsis" #Genus
 date = "2020-04-22"
-path_localgb = f"{genus_term}_{date}.json"
-path_localgb_xlsx = f"{genus_term}_{date}.xlsx"
+path_out = f"{genus_term}_{date}"
+path_localgb = f"{path_out}.json"
+path_localgb_xlsx = f"{path_out}.xlsx"
 max_len = 5000 # Excluding too long (genomic) Sequences
 additional_terms = []
 struct = []
-path_out = f"{genus_term}_{date}"
 
+# Argument input for running in commandline mode
+parser = argparse.ArgumentParser(description='Download genbank records')
+parser.add_argument("--email", "-e", help = "entrez email", type=str)
+parser.add_argument("--genus", "-g", help = "genus to find", type=str)
+parser.add_argument("--additional", "-a", nargs='*', help = "additional terms", type=str)
+parser.add_argument("--out", "-o", help = "out path", type=str)
+parser.add_argument("--max", "-m", help = "maximum length", type=int)
 
+args = parser.parse_args()
+
+if args.email != None:
+	email = args.email
+
+if args.genus != None:
+	genus_term = args.genus
+
+if args.additional != None:
+	additional_term = list(args.additional)
+
+if args.out != None:
+	path_out = args.out
+
+if args.max != None:
+	max_len = args.max
+
+# Make temporary directory if not exists
 
 try:
 	os.mkdir("./tmp")
 except:
 	pass
+
 #Download all seqs from NCBI with genus Penicillium and save into json
 Gen.NCBI_Download(email, genus_term, path_localgb)
 
